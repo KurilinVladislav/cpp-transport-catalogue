14f083baced14b5f04843bb8a37fd2b2971052d2
Владислав <95681197+KurilinVladislav@users.noreply.github.com>
2022-09-16 13:22:32 +0300

    Update request_handler.h

--- transport-catalogue/request_handler.h
+++ transport-catalogue/request_handler.h
@@ -9,20 +9,16 @@ namespace transport {
     
 class RequestHandler {
 public:
-    // MapRenderer понадобится в следующей части итогового проекта
     RequestHandler(const TransportCatalogue& db, const renderer::MapRenderer& renderer);
 
     // Возвращает информацию о маршруте (запрос Bus)
     std::optional<BusInfo> GetBusStat(const std::string_view& bus_name) const;
 
-    // Возвращает маршруты, проходящие через
     std::optional<std::unordered_set<std::string_view>> GetBusesByStop(const std::string_view& stop_name) const;
 
-    // Этот метод будет нужен в следующей части итогового проекта
     svg::Document RenderMap() const;
 
 private:
-    // RequestHandler использует агрегацию объектов "Транспортный Справочник" и "Визуализатор Карты"
     const TransportCatalogue& db_;
     const renderer::MapRenderer& renderer_;
 };
